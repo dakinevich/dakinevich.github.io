46b607525558339a5eb2aa7410849983c4cf2d73
ivan <ivan7855060@gmail.com>
2023-12-02 08:34:56 +0300

    card start

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 var header = document.querySelector(".section-header");
-var h = document.querySelector(".section-header__h");
 var p = document.querySelector(".header__p");
+var chapter = document.querySelector(".btn-selected").innerText;
 
 window.addEventListener("scroll", ()=>{
-    header.style.backgroundPosition = "center "+(scrollY/3)+"px";
-    var height = h.getBoundingClientRect().top;
+    header.style.backgroundPosition = "center "+(scrollY/2)+"px";
+    var height = document.querySelector(".section-header__list").getBoundingClientRect().top;
     // these are relative to the viewport, i.e. the window
     console.log(height);
-    /*if (height < -50){
+    if (height < -0){
         p.style.transform =  "translate(0,0)";
+        p.innerText = document.querySelector(".btn-selected").innerText;
     }
     else{
-        p.style.transform = "translate(0,70px)";
-    }*/
+        //p.style.transform = "translate(0,70px)";
+        p.innerText = "Кто прочел тот осел";
+    }
 })
 
+function chapter_click(e){
+    document.querySelector(".btn-selected").classList.remove("btn-selected");
+    e.classList.add("btn-selected");
+}
+
+function apparat_click(e, index){
+    //alert(index);
+    document.querySelector(".apparat-section").style.visibility = "visible";
+}
+
+
+function exit_apparat(e){
+    //alert(index);
+    document.querySelector(".apparat-section").style.visibility = "";
+}
 
