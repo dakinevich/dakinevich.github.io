b01bf9850083e88cbd0eb89bedf06912edd5b36c
ivan <ivan7855060@gmail.com>
2023-12-11 14:33:01 +0300

    hotfix

--- scripts/moon_move.js
+++ scripts/moon_move.js
@@ -2,8 +2,4 @@ var header = document.querySelector(".section-header");
 
 window.addEventListener("scroll", ()=>{
     header.style.backgroundPosition = "center "+(scrollY/2)+"px";
-    var height = document.querySelector(".section-header__list").getBoundingClientRect().top;
-    // these are relative to the viewport, i.e. the window
-    //console.log(height);
-    
 })
