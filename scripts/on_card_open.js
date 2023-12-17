402b9a5a5609ab606237ec93348bd495bc0a25b5
ivan <ivan7855060@gmail.com>
2023-12-18 01:18:59 +0300

    more details and apparat info, footer

--- scripts/on_card_open.js
+++ scripts/on_card_open.js
@@ -11,12 +11,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 this.querySelector(".apparat-section__header-h").innerHTML = item.name;
         
                 let img = document.createElement('img');
+                img.className = 'apparat-section__title-img';
                 img.src = item.img;
                 element.appendChild(img)
 
                 let p = document.createElement('p');
+                p.className = 'apparat-section__title-p';
                 p.innerHTML = item.info;
                 element.appendChild(p)
+                for(var i in item.article){
+                    if (item.article[i].img){
+                        let img = document.createElement('img');
+                        img.className = 'apparat-section__article-img';
+                        img.src = item.article[i].img;
+                        element.appendChild(img)
+
+                    }
+                    if (item.article[i].text){
+                        let p = document.createElement('p');
+                        p.className = 'apparat-section__article-p';
+                        p.innerHTML = item.article[i].text;
+                        element.appendChild(p)
+
+                    }
+
+                }
 
             }
 
